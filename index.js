c00a45a4f543b69785fae5b19bde365d52c49afe
gitjoshu <joshua.munoz.barbero@gmail.com>
2022-08-20 14:17:58 +0200

    oak

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { Application, Router } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 
-serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
+const app = new Application();
+const tasks = ['Task 1', 'Task 2', 'Task 3'];
+
+const tasksRoutes = new Router()
+  .get("/", (ctx) => {
+    ctx.response.body = "Welcome to my API";
+  })
+  .get("/tasks", (ctx) => {
+    ctx.response.headers.set("Content-Type", "application/json");
+    ctx.response.body = tasks;
+  })
+  .post("/tasks", async (ctx) => {
+    const newTask = await ctx.request.body().value;
+    tasks.push(newTask);
+    ctx.response.body = newTask;
   });
-});
+
+app.use(tasksRoutes.routes());
+app.use(tasksRoutes.allowedMethods());
+
+console.log("Server running on port 8000");
+await app.listen({ port: 8000 });
